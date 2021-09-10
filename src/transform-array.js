939cabc11b5af43ed87f28c948b2e33dedd9465e
Artur Mingazhov <arthurming7@gmail.com>
2021-09-10 17:16:20 +0300

    feat: add solution for transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,55 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!(arr instanceof Array)) {
+    throw Error("'arr' parameter must be an instance of the Array!")
+  }
+  let localArr = [...arr]
+
+  for (let i = 0; i < localArr.length; i++) {
+
+    const item = localArr[i]
+
+    if (item === '--discard-prev') {
+      if (i === 0) {
+        localArr.splice(i, 1)
+        i--
+      } else {
+        localArr.splice(i - 1, 2)
+        i -= 2
+      }
+    }
+    if (item === '--double-prev') {
+      if (i === 0) {
+        localArr.splice(i, 1)
+        i--
+      }
+      else {
+        localArr = [...localArr.slice(0, i), ...localArr.slice(i - 1, i), ...localArr.slice(i + 1)]
+        i++
+
+      }
+    }
+    if (item === '--double-next') {
+      if (i + 1 === localArr.length) {
+        localArr.splice(i, 1)
+      } else {
+        localArr = [...localArr.slice(0, i), ...localArr.slice(i + 1, i + 2), ...localArr.slice(i + 1)]
+      }
+    }
+    if (item === '--discard-next') {
+      if (i + 1 === localArr.length) {
+        localArr.splice(i, 1)
+      } else {
+        if (localArr[i + 2] === '--double-prev' || localArr[i + 2] === '--discard-prev') {
+          localArr = [...localArr.slice(0, i), ...localArr.slice(i + 3)]
+        } else {
+          localArr = [...localArr.slice(0, i), ...localArr.slice(i + 2)]
+        }
+        i--
+      }
+    }
+  }
+  return localArr
 }
