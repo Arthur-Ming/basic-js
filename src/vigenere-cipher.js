e869f8cc59d97a880693fe7a3959231006f78a9c
Artur Mingazhov <arthurming7@gmail.com>
2021-09-12 13:54:56 +0300

    feat: add solution for vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,65 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  vigenereTable = [];
+
+  alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+
+  creatVigenereTable() {
+    this.vigenereTable = []
+    this.alphabet.forEach(() => {
+      this.vigenereTable.push([... this.alphabet])
+      this.alphabet.push(this.alphabet.shift())
+    })
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  creatKey(message, key) {
+    let n = ''
+    for (let i = 0, x = 0; i < message.length; i++, x++) {
+      const sign = message[i].toUpperCase()
+      if (this.alphabet.includes(sign)) {
+        if (x >= key.length) x = 0
+        n += key[x]
+      }
+      else {
+        n += message[i]
+        x--
+      }
+    }
+    return n
+  }
+
+  constructor(direct = true) {
+    this.direct = direct
+    this.creatVigenereTable()
+  }
+  encrypt(message, key) {
+    if (!message || !key) throw Error('Incorrect arguments!')
+    const createdkey = this.creatKey(message, key)
+    let encrypted = ''
+    for (let i = 0; i < message.length; i++) {
+      const row = this.alphabet.indexOf(message[i].toUpperCase())
+      const column = this.alphabet.indexOf(createdkey[i].toUpperCase())
+      row === -1 ? encrypted += message[i] : encrypted += this.vigenereTable[row][column]
+    }
+    return this.direct ? encrypted : encrypted.split('').reverse().join('')
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw Error('Incorrect arguments!')
+    const createdkey = this.creatKey(message, key)
+    let decrypted = ''
+    for (let i = 0; i < message.length; i++) {
+      const column = this.alphabet.indexOf(createdkey[i].toUpperCase())
+      if (column !== - 1) {
+        let row = 0
+        while (this.vigenereTable[row][column] !== message[i].toUpperCase()) {
+          row++
+        }
+        decrypted += this.vigenereTable[row][0]
+      } else {
+        decrypted += createdkey[i]
+      }
+    }
+    return this.direct ? decrypted : decrypted.split('').reverse().join('')
   }
 }
